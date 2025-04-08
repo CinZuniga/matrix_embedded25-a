a5778fbf338b5c9c91f87eb0b081b526d7afad47
CinZuniga <czuniga10@alumnos.uaq.mx>
2025-04-07 18:25:51 -0600

    Add the read matrix function

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ A simple matrix project
 
 ```bash 
 python3 main.py
-``` 
+```
+
+## TODO:
+1. Read a matrix from the stdin using the following format:
+    - The first line is the width and the height of the matrix
+    - The following lines are the rows of the matrix, separated by spaces
+    - print the matrix
+  
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 
+def read():
+    line = input().split()
+    rows,cols = int(line[0]), int(line[1])
+
+    m = []
+    for i in range(rows):
+        line = input().split()
+        row =list( map(int, line))
+        m.append(row)
+    return m
+
 def main():
     print("Simple Matrix")
-    pass
+    m = read()
+    print(m)
 
-if __name__=="__main__"":
+if __name__=="__main__":
     main()
 
